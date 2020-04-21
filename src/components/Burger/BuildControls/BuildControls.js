03dca06e2ffe344bf591609852f1c0e913ae7826
micgogi <rahul.gogyani@gmail.com>
2020-04-21 19:53:06 +0530

    add and remove items functionality added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -16,7 +16,8 @@ const buildControls = (props) => {
            {controls.map(ctrl=>(
                <BuildControl key={ctrl.label} label={ctrl.label} 
                added={()=>props.ingredientAdded(ctrl.type)}
-               />
+               removed={()=>props.ingredientRemoved(ctrl.type)}
+               disabled={props.disabled[ctrl.type]}/>
            ))}
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,14 +39,35 @@ export default class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) =>{
-    
+        const oldCount = this.state.ingredients[type];
+        if(oldCount<=0){
+            return;
+        }
+        const updatedCounted = oldCount-1;
+        const updatedIngredients={
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCounted;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice+priceDeduction;
+        this.setState({totalPrice:newPrice,ingredients:updatedIngredients});
     }
     render() {
+
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo){
+            disabledInfo[key] = disabledInfo[key]<=0
+        }
         return (
              <Auxiliary >
 
             <Burger ingredients={this.state.ingredients} > </Burger> 
-            <BuildControls ingredientAdded={this.addIngredientHandler}/>
+            <BuildControls ingredientAdded={this.addIngredientHandler}
+                            ingredientRemoved={this.removeIngredientHandler}
+                            disabled={disabledInfo}/>
             
             </Auxiliary>
         )
